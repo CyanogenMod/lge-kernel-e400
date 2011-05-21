c8d6129d96f059ae0063b00dd33168fb968eae92
Han Jun-Yeong <junyeong.han@lge.com>
2011-05-21 17:51:30 +0900

    [M3] Add the board file for lcd devices.   - lcd devices.   - Please check data and configuration and modify them fot M3 lcd.

---
+++ arch/arm/mach-msm/lge/board-m3-panel.c
@@ -0,0 +1,311 @@
+#include <linux/err.h>
+#include <linux/gpio.h>
+
+#include <asm/mach-types.h>
+
+#include <mach/msm_rpcrouter.h>
+#include <mach/rpc_pmapp.h>
+#include <mach/board.h>
+
+#include "devices.h"
+#include "board-m3.h"
+
+#define LCDC_CONFIG_PROC          21
+#define LCDC_UN_CONFIG_PROC       22
+#define LCDC_API_PROG             0x30000066
+#define LCDC_API_VERS             0x00010001
+
+static struct msm_rpc_endpoint *lcdc_ep;
+
+static int msm_fb_lcdc_config(int on)
+{
+	int rc = 0;
+	struct rpc_request_hdr hdr;
+
+	if (on)
+		pr_info("lcdc config\n");
+	else
+		pr_info("lcdc un-config\n");
+
+	lcdc_ep = msm_rpc_connect_compatible(LCDC_API_PROG, LCDC_API_VERS, 0);
+
+	if (IS_ERR(lcdc_ep)) {
+		printk(KERN_ERR "%s: msm_rpc_connect failed! rc = %ld\n",
+			__func__, PTR_ERR(lcdc_ep));
+		return -EINVAL;
+	}
+
+	rc = msm_rpc_call(lcdc_ep,
+		(on) ? LCDC_CONFIG_PROC : LCDC_UN_CONFIG_PROC,
+		&hdr, sizeof(hdr), 5 * HZ);
+
+	if (rc)
+		printk(KERN_ERR
+			"%s: msm_rpc_call failed! rc = %d\n", __func__, rc);
+
+	msm_rpc_close(lcdc_ep);
+	return rc;
+}
+
+static uint32_t lcdc_gpio_initialized;
+
+static void lcdc_toshiba_gpio_init(void)
+{
+	if (!lcdc_gpio_initialized) {
+		if (gpio_request(GPIO_SPI_CLK, "spi_clk")) {
+			pr_err("failed to request gpio spi_clk\n");
+			goto fail_gpio;
+		}
+		if (gpio_request(GPIO_SPI_CS0_N, "spi_cs")) {
+			pr_err("failed to request gpio spi_cs0_N\n");
+			goto fail_gpio;
+		}
+		if (gpio_request(GPIO_SPI_MOSI, "spi_mosi")) {
+			pr_err("failed to request gpio spi_mosi\n");
+			goto fail_gpio;
+		}
+		if (gpio_request(GPIO_SPI_MISO, "spi_miso")) {
+			pr_err("failed to request gpio spi_miso\n");
+			goto fail_gpio;
+		}
+		if (gpio_request(GPIO_DISPLAY_PWR_EN, "gpio_disp_pwr")) {
+			pr_err("failed to request gpio_disp_pwr\n");
+			goto fail_gpio;
+		}
+		if (gpio_request(GPIO_BACKLIGHT_EN, "gpio_bkl_en")) {
+			pr_err("failed to request gpio_bkl_en\n");
+			goto fail_gpio;
+		}
+		lcdc_gpio_initialized = 1;
+		return;
+	}
+fail_gpio:
+	lcdc_gpio_initialized = 0;
+}
+
+static uint32_t lcdc_gpio_table[] = {
+	GPIO_SPI_CLK,
+	GPIO_SPI_CS0_N,
+	GPIO_SPI_MOSI,
+	GPIO_DISPLAY_PWR_EN,
+	GPIO_BACKLIGHT_EN,
+	GPIO_SPI_MISO,
+};
+
+static void config_lcdc_gpio_table(uint32_t *table, int len, unsigned enable)
+{
+	int n;
+
+	if (lcdc_gpio_initialized) {
+		/* All are IO Expander GPIOs */
+		for (n = 0; n < (len - 1); n++)
+			gpio_direction_output(table[n], 1);
+	}
+}
+
+static void lcdc_toshiba_config_gpios(int enable)
+{
+	config_lcdc_gpio_table(lcdc_gpio_table,
+		ARRAY_SIZE(lcdc_gpio_table), enable);
+}
+
+static int msm_fb_lcdc_power_save(int on)
+{
+	/* struct vreg *vreg[ARRAY_SIZE(msm_fb_lcdc_vreg)]; */
+	int rc = 0;
+
+	/* Doing the init of the LCDC GPIOs very late as they are from
+		an I2C-controlled IO Expander */
+	lcdc_toshiba_gpio_init();
+
+	if (lcdc_gpio_initialized) {
+		gpio_set_value_cansleep(GPIO_DISPLAY_PWR_EN, on);
+		gpio_set_value_cansleep(GPIO_BACKLIGHT_EN, on);
+	}
+
+	pmapp_disp_backlight_init();
+	rc = pmapp_disp_backlight_set_brightness(100);
+
+	return 0;
+}
+
+static struct lcdc_platform_data lcdc_pdata = {
+	.lcdc_gpio_config = msm_fb_lcdc_config,
+	.lcdc_power_save   = msm_fb_lcdc_power_save,
+};
+
+static int lcd_panel_spi_gpio_num[] = {
+		GPIO_SPI_MOSI,  /* spi_sdi */
+		GPIO_SPI_MISO,  /* spi_sdoi */
+		GPIO_SPI_CLK,   /* spi_clk */
+		GPIO_SPI_CS0_N, /* spi_cs  */
+};
+
+static struct msm_panel_common_pdata lcdc_toshiba_panel_data = {
+	.panel_config_gpio = lcdc_toshiba_config_gpios,
+	.gpio_num	  = lcd_panel_spi_gpio_num,
+};
+
+static struct platform_device lcdc_toshiba_panel_device = {
+	.name   = "lcdc_toshiba_fwvga_pt",
+	.id     = 0,
+	.dev    = {
+		.platform_data = &lcdc_toshiba_panel_data,
+	}
+};
+
+#ifdef CONFIG_FB_MSM_MIPI_DSI
+static struct platform_device mipi_dsi_renesas_panel_device = {
+	.name = "mipi_renesas",
+	.id = 0,
+};
+#endif
+
+/* input platform device */
+static struct platform_device *m3_panel_devices[] __initdata = {
+	&lcdc_toshiba_panel_device,
+#ifdef CONFIG_FB_MSM_MIPI_DSI
+	&mipi_dsi_renesas_panel_device,
+#endif
+};
+
+static struct msm_panel_common_pdata mdp_pdata = {
+	.gpio = 97,
+};
+
+#define GPIO_LCDC_BRDG_PD	128
+#define GPIO_LCDC_BRDG_RESET_N	129
+static unsigned mipi_dsi_gpio[] = {
+	GPIO_CFG(GPIO_LCDC_BRDG_RESET_N, 0, GPIO_CFG_OUTPUT, GPIO_CFG_NO_PULL,
+		GPIO_CFG_2MA),       /* LCDC_BRDG_RESET_N */
+};
+
+static int msm_fb_dsi_client_reset(void)
+{
+	int rc = 0;
+
+	rc = gpio_request(GPIO_LCDC_BRDG_RESET_N, "lcdc_brdg_reset_n");
+
+	if (rc < 0) {
+		pr_err("failed to request lcd brdg reset\n");
+		return rc;
+	}
+
+	rc = gpio_tlmm_config(mipi_dsi_gpio[0], GPIO_CFG_ENABLE);
+	if (rc) {
+		pr_err("Failed to enable LCDC Bridge reset enable\n");
+		goto gpio_error;
+	}
+
+	rc = gpio_direction_output(GPIO_LCDC_BRDG_RESET_N, 1);
+	if (!rc) {
+		gpio_set_value_cansleep(GPIO_LCDC_BRDG_RESET_N, 0);
+		msleep(20);
+		gpio_set_value_cansleep(GPIO_LCDC_BRDG_RESET_N, 1);
+		return rc;
+	} else {
+		goto gpio_error;
+	}
+
+gpio_error:
+	pr_err("Failed GPIO bridge reset\n");
+	gpio_free(GPIO_LCDC_BRDG_RESET_N);
+	return rc;
+}
+
+static int dsi_gpio_initialized;
+
+static int mipi_dsi_panel_power(int on)
+{
+	int rc = 0;
+
+	if (!on)
+		return rc;
+
+	/* I2C-controlled GPIO Expander -init of the GPIOs very late */
+	if (!dsi_gpio_initialized) {
+		pmapp_disp_backlight_init();
+
+		if (pmapp_disp_backlight_set_brightness(100))
+			pr_err("display backlight set brightness failed\n");
+
+		if (machine_is_msm7x27a_surf()) {
+			rc = gpio_request(GPIO_DISPLAY_PWR_EN, "gpio_disp_pwr");
+			if (rc < 0) {
+				pr_err("failed to request gpio_disp_pwr\n");
+				return rc;
+			}
+
+			if (gpio_request(GPIO_BACKLIGHT_EN, "gpio_bkl_en")) {
+				pr_err("failed to request gpio_bkl_en\n");
+				goto fail_gpio1;
+			}
+
+			if (!gpio_direction_output(GPIO_DISPLAY_PWR_EN, 1)) {
+				gpio_set_value_cansleep(GPIO_DISPLAY_PWR_EN,
+					on);
+			} else {
+				pr_err("failed to enable display pwr\n");
+				goto fail_gpio2;
+			}
+
+			if (!gpio_direction_output(GPIO_BACKLIGHT_EN, 1)) {
+				gpio_set_value_cansleep(GPIO_BACKLIGHT_EN, on);
+				return rc;
+			} else {
+				pr_err("failed to enable backlight\n");
+				goto fail_gpio2;
+			}
+
+fail_gpio2:
+		gpio_free(GPIO_BACKLIGHT_EN);
+fail_gpio1:
+		gpio_free(GPIO_DISPLAY_PWR_EN);
+
+		} else {
+			rc = gpio_request(GPIO_FFA_LCD_PWR_EN_N,
+				"gpio_disp_pwr");
+			if (rc < 0) {
+				pr_err("failed to request lcd pwr\n");
+				return rc;
+			}
+			if (!gpio_direction_output(GPIO_FFA_LCD_PWR_EN_N,
+				1)) {
+				gpio_set_value_cansleep(GPIO_FFA_LCD_PWR_EN_N,
+					on);
+				return rc;
+			} else {
+				pr_err("failed to enable lcd pwr\n");
+				return rc;
+			}
+
+		}
+	}
+
+	return rc;
+}
+
+#define MDP_303_VSYNC_GPIO 97
+
+#ifdef CONFIG_FB_MSM_MDP303
+static struct mipi_dsi_platform_data mipi_dsi_pdata = {
+	.vsync_gpio = MDP_303_VSYNC_GPIO,
+	.dsi_power_save   = mipi_dsi_panel_power,
+	.dsi_client_reset = msm_fb_dsi_client_reset,
+};
+#endif
+
+static void __init msm_fb_add_devices(void)
+{
+	msm_fb_register_device("mdp", &mdp_pdata);
+	msm_fb_register_device("lcdc", &lcdc_pdata);
+	msm_fb_register_device("mipi_dsi", &mipi_dsi_pdata);
+}
+
+/* common functions */
+void __init lge_add_lcd_devices(void)
+{
+	platform_add_devices(m3_panel_devices, ARRAY_SIZE(m3_panel_devices));
+	msm_fb_add_devices();
+}
+
--- arch/arm/mach-msm/lge/board-m3.c
+++ arch/arm/mach-msm/lge/board-m3.c
@@ -18,7 +18,6 @@
 #include <mach/msm_iomap.h>
 #include <mach/msm_hsusb.h>
 #include <mach/rpc_hsusb.h>
-#include <mach/rpc_pmapp.h>
 #include <mach/usbdiag.h>
 #include <mach/usb_gadget_fserial.h>
 #include <mach/msm_serial_hs.h>
@@ -48,72 +47,13 @@
 
 #include <mach/board_lge.h>
 
+#include "board-m3.h"
+
 #define PMEM_KERNEL_EBI1_SIZE	0x1C000
 #define MSM_PMEM_AUDIO_SIZE	0x5B000
 #define BAHAMA_SLAVE_ID_FM_ADDR         0x2A
 #define BAHAMA_SLAVE_ID_QMEMBIST_ADDR   0x7B
 #define FM_GPIO	83
-
-enum {
-	GPIO_EXPANDER_IRQ_BASE	= NR_MSM_IRQS + NR_GPIO_IRQS,
-	GPIO_EXPANDER_GPIO_BASE	= NR_MSM_GPIOS,
-	/* SURF expander */
-	GPIO_SURF_EXPANDER_BASE	= GPIO_EXPANDER_GPIO_BASE,
-	GPIO_BT_SYS_REST_EN	= GPIO_SURF_EXPANDER_BASE,
-	GPIO_WLAN_EXT_POR_N,
-	GPIO_DISPLAY_PWR_EN,
-	GPIO_BACKLIGHT_EN,
-	GPIO_PRESSURE_XCLR,
-	GPIO_VREG_S3_EXP,
-	GPIO_UBM2M_PWRDWN,
-	GPIO_ETM_MODE_CS_N,
-	GPIO_HOST_VBUS_EN,
-	GPIO_SPI_MOSI,
-	GPIO_SPI_MISO,
-	GPIO_SPI_CLK,
-	GPIO_SPI_CS0_N,
-	GPIO_SURF_EXPANDER_IO13,
-	GPIO_SURF_EXPANDER_IO14,
-	GPIO_SURF_EXPANDER_IO15,
-	/* Camera expander on SURF */
-	GPIO_SURF_CAM_EXPANDER_BASE	= GPIO_SURF_EXPANDER_BASE + 16,
-	GPIO_SURF_CAM_GP_STROBE_READY	= GPIO_SURF_CAM_EXPANDER_BASE,
-	GPIO_SURF_CAM_GP_AFBUSY,
-	GPIO_SURF_CAM_GP_CAM_PWDN,
-	GPIO_SURF_CAM_GP_CAM1MP_XCLR,
-	GPIO_SURF_CAM_GP_CAMIF_RESET_N,
-	GPIO_SURF_CAM_GP_STROBE_CE,
-	GPIO_SURF_CAM_GP_LED_EN1,
-	GPIO_SURF_CAM_GP_LED_EN2,
-	/* FFA expander */
-	GPIO_FFA_EXPANDER_BASE	= GPIO_SURF_CAM_EXPANDER_BASE + 8,
-	GPIO_FFA_BT_SYS_REST_EN	= GPIO_FFA_EXPANDER_BASE,
-	GPIO_FFA_WLAN_EXT_POR_N,
-	GPIO_FFA_LCD_PWR_EN_N,
-	GPIO_FFA_GPIO_EXP_03,
-	GPIO_FFA_ALTIMETER_XCLR_N,
-	GPIO_FFA_GPIO_EXP_05,
-	GPIO_FFA_GPIO_EXP_06,
-	GPIO_FFA_GPIO_EXP_07,
-	GPIO_FFA_5V_BOOST_EN,
-	GPIO_FFA_EXPANDER_IO09,
-	GPIO_FFA_EXPANDER_IO10,
-	GPIO_FFA_EXPANDER_IO11,
-	GPIO_FFA_EXPANDER_IO12,
-	GPIO_FFA_EXPANDER_IO13,
-	GPIO_FFA_EXPANDER_IO14,
-	GPIO_FFA_EXPANDER_IO15,
-	/* Camera expander on FFA */
-	GPIO_FFA_CAM_EXPANDER_BASE	= GPIO_FFA_EXPANDER_BASE + 16,
-	GPIO_FFA_CAM_EXP_GPIO_00	= GPIO_FFA_CAM_EXPANDER_BASE,
-	GPIO_FFA_CAM_EXP_GPIO_O1,
-	GPIO_FFA_CAM_5MP_PWDN_VCM,
-	GPIO_FFA_CAM_1MP_XCLR,
-	GPIO_FFA_CAM_5MP_SHDN_N,
-	GPIO_FFA_CAM_EXP_GPIO_05,
-	GPIO_FFA_LED_DRV_EN1,
-	GPIO_FFA_LED_DRV_EN2,
-};
 
 #if defined(CONFIG_GPIO_SX150X)
 enum {
@@ -1153,157 +1093,6 @@ static struct msm_pm_platform_data msm7x27a_pm_data[MSM_PM_SLEEP_MODE_NR] = {
 	},
 };
 
-#define LCDC_CONFIG_PROC          21
-#define LCDC_UN_CONFIG_PROC       22
-#define LCDC_API_PROG             0x30000066
-#define LCDC_API_VERS             0x00010001
-
-static struct msm_rpc_endpoint *lcdc_ep;
-
-static int msm_fb_lcdc_config(int on)
-{
-	int rc = 0;
-	struct rpc_request_hdr hdr;
-
-	if (on)
-		pr_info("lcdc config\n");
-	else
-		pr_info("lcdc un-config\n");
-
-	lcdc_ep = msm_rpc_connect_compatible(LCDC_API_PROG, LCDC_API_VERS, 0);
-
-	if (IS_ERR(lcdc_ep)) {
-		printk(KERN_ERR "%s: msm_rpc_connect failed! rc = %ld\n",
-			__func__, PTR_ERR(lcdc_ep));
-		return -EINVAL;
-	}
-
-	rc = msm_rpc_call(lcdc_ep,
-		(on) ? LCDC_CONFIG_PROC : LCDC_UN_CONFIG_PROC,
-		&hdr, sizeof(hdr), 5 * HZ);
-
-	if (rc)
-		printk(KERN_ERR
-			"%s: msm_rpc_call failed! rc = %d\n", __func__, rc);
-
-	msm_rpc_close(lcdc_ep);
-	return rc;
-}
-
-static uint32_t lcdc_gpio_initialized;
-
-static void lcdc_toshiba_gpio_init(void)
-{
-	if (!lcdc_gpio_initialized) {
-		if (gpio_request(GPIO_SPI_CLK, "spi_clk")) {
-			pr_err("failed to request gpio spi_clk\n");
-			goto fail_gpio;
-		}
-		if (gpio_request(GPIO_SPI_CS0_N, "spi_cs")) {
-			pr_err("failed to request gpio spi_cs0_N\n");
-			goto fail_gpio;
-		}
-		if (gpio_request(GPIO_SPI_MOSI, "spi_mosi")) {
-			pr_err("failed to request gpio spi_mosi\n");
-			goto fail_gpio;
-		}
-		if (gpio_request(GPIO_SPI_MISO, "spi_miso")) {
-			pr_err("failed to request gpio spi_miso\n");
-			goto fail_gpio;
-		}
-		if (gpio_request(GPIO_DISPLAY_PWR_EN, "gpio_disp_pwr")) {
-			pr_err("failed to request gpio_disp_pwr\n");
-			goto fail_gpio;
-		}
-		if (gpio_request(GPIO_BACKLIGHT_EN, "gpio_bkl_en")) {
-			pr_err("failed to request gpio_bkl_en\n");
-			goto fail_gpio;
-		}
-		lcdc_gpio_initialized = 1;
-		return;
-	}
-fail_gpio:
-	lcdc_gpio_initialized = 0;
-}
-
-static uint32_t lcdc_gpio_table[] = {
-	GPIO_SPI_CLK,
-	GPIO_SPI_CS0_N,
-	GPIO_SPI_MOSI,
-	GPIO_DISPLAY_PWR_EN,
-	GPIO_BACKLIGHT_EN,
-	GPIO_SPI_MISO,
-};
-
-static void config_lcdc_gpio_table(uint32_t *table, int len, unsigned enable)
-{
-	int n;
-
-	if (lcdc_gpio_initialized) {
-		/* All are IO Expander GPIOs */
-		for (n = 0; n < (len - 1); n++)
-			gpio_direction_output(table[n], 1);
-	}
-}
-
-static void lcdc_toshiba_config_gpios(int enable)
-{
-	config_lcdc_gpio_table(lcdc_gpio_table,
-		ARRAY_SIZE(lcdc_gpio_table), enable);
-}
-
-static int msm_fb_lcdc_power_save(int on)
-{
-	/* struct vreg *vreg[ARRAY_SIZE(msm_fb_lcdc_vreg)]; */
-	int rc = 0;
-
-	/* Doing the init of the LCDC GPIOs very late as they are from
-		an I2C-controlled IO Expander */
-	lcdc_toshiba_gpio_init();
-
-	if (lcdc_gpio_initialized) {
-		gpio_set_value_cansleep(GPIO_DISPLAY_PWR_EN, on);
-		gpio_set_value_cansleep(GPIO_BACKLIGHT_EN, on);
-	}
-
-	pmapp_disp_backlight_init();
-	rc = pmapp_disp_backlight_set_brightness(100);
-
-	return 0;
-}
-
-static struct lcdc_platform_data lcdc_pdata = {
-	.lcdc_gpio_config = msm_fb_lcdc_config,
-	.lcdc_power_save   = msm_fb_lcdc_power_save,
-};
-
-static int lcd_panel_spi_gpio_num[] = {
-		GPIO_SPI_MOSI,  /* spi_sdi */
-		GPIO_SPI_MISO,  /* spi_sdoi */
-		GPIO_SPI_CLK,   /* spi_clk */
-		GPIO_SPI_CS0_N, /* spi_cs  */
-};
-
-static struct msm_panel_common_pdata lcdc_toshiba_panel_data = {
-	.panel_config_gpio = lcdc_toshiba_config_gpios,
-	.gpio_num	  = lcd_panel_spi_gpio_num,
-};
-
-static struct platform_device lcdc_toshiba_panel_device = {
-	.name   = "lcdc_toshiba_fwvga_pt",
-	.id     = 0,
-	.dev    = {
-		.platform_data = &lcdc_toshiba_panel_data,
-	}
-};
-
-#ifdef CONFIG_FB_MSM_MIPI_DSI
-static struct platform_device mipi_dsi_renesas_panel_device = {
-	.name = "mipi_renesas",
-	.id = 0,
-};
-#endif
-
 static struct smsc911x_platform_config smsc911x_config = {
 	.irq_polarity	= SMSC911X_IRQ_POLARITY_ACTIVE_HIGH,
 	.irq_type	= SMSC911X_IRQ_TYPE_PUSH_PULL,
@@ -1766,7 +1555,6 @@ static struct platform_device *surf_ffa_devices[] __initdata = {
 	&rndis_device,
 	&usb_diag_device,
 	&usb_gadget_fserial_device,
-	&lcdc_toshiba_panel_device,
 	&smsc911x_device,
 #ifdef CONFIG_S5K4E1
 	&msm_camera_sensor_s5k4e1,
@@ -1780,9 +1568,6 @@ static struct platform_device *surf_ffa_devices[] __initdata = {
 #ifdef CONFIG_MT9E013
 	&msm_camera_sensor_mt9e013,
 #endif
-#ifdef CONFIG_FB_MSM_MIPI_DSI
-	&mipi_dsi_renesas_panel_device,
-#endif
 	&msm_kgsl_3d0,
 #ifdef CONFIG_BT
 	&msm_bt_power_device,
@@ -1793,139 +1578,6 @@ static void __init msm_device_i2c_init(void)
 {
 	msm_gsbi0_qup_i2c_device.dev.platform_data = &msm_gsbi0_qup_i2c_pdata;
 	msm_gsbi1_qup_i2c_device.dev.platform_data = &msm_gsbi1_qup_i2c_pdata;
-}
-
-static struct msm_panel_common_pdata mdp_pdata = {
-	.gpio = 97,
-};
-
-#define GPIO_LCDC_BRDG_PD	128
-#define GPIO_LCDC_BRDG_RESET_N	129
-static unsigned mipi_dsi_gpio[] = {
-	GPIO_CFG(GPIO_LCDC_BRDG_RESET_N, 0, GPIO_CFG_OUTPUT, GPIO_CFG_NO_PULL,
-		GPIO_CFG_2MA),       /* LCDC_BRDG_RESET_N */
-};
-
-static int msm_fb_dsi_client_reset(void)
-{
-	int rc = 0;
-
-	rc = gpio_request(GPIO_LCDC_BRDG_RESET_N, "lcdc_brdg_reset_n");
-
-	if (rc < 0) {
-		pr_err("failed to request lcd brdg reset\n");
-		return rc;
-	}
-
-	rc = gpio_tlmm_config(mipi_dsi_gpio[0], GPIO_CFG_ENABLE);
-	if (rc) {
-		pr_err("Failed to enable LCDC Bridge reset enable\n");
-		goto gpio_error;
-	}
-
-	rc = gpio_direction_output(GPIO_LCDC_BRDG_RESET_N, 1);
-	if (!rc) {
-		gpio_set_value_cansleep(GPIO_LCDC_BRDG_RESET_N, 0);
-		msleep(20);
-		gpio_set_value_cansleep(GPIO_LCDC_BRDG_RESET_N, 1);
-		return rc;
-	} else {
-		goto gpio_error;
-	}
-
-gpio_error:
-	pr_err("Failed GPIO bridge reset\n");
-	gpio_free(GPIO_LCDC_BRDG_RESET_N);
-	return rc;
-}
-
-static int dsi_gpio_initialized;
-
-static int mipi_dsi_panel_power(int on)
-{
-	int rc = 0;
-
-	if (!on)
-		return rc;
-
-	/* I2C-controlled GPIO Expander -init of the GPIOs very late */
-	if (!dsi_gpio_initialized) {
-		pmapp_disp_backlight_init();
-
-		if (pmapp_disp_backlight_set_brightness(100))
-			pr_err("display backlight set brightness failed\n");
-
-		if (machine_is_msm7x27a_surf()) {
-			rc = gpio_request(GPIO_DISPLAY_PWR_EN, "gpio_disp_pwr");
-			if (rc < 0) {
-				pr_err("failed to request gpio_disp_pwr\n");
-				return rc;
-			}
-
-			if (gpio_request(GPIO_BACKLIGHT_EN, "gpio_bkl_en")) {
-				pr_err("failed to request gpio_bkl_en\n");
-				goto fail_gpio1;
-			}
-
-			if (!gpio_direction_output(GPIO_DISPLAY_PWR_EN, 1)) {
-				gpio_set_value_cansleep(GPIO_DISPLAY_PWR_EN,
-					on);
-			} else {
-				pr_err("failed to enable display pwr\n");
-				goto fail_gpio2;
-			}
-
-			if (!gpio_direction_output(GPIO_BACKLIGHT_EN, 1)) {
-				gpio_set_value_cansleep(GPIO_BACKLIGHT_EN, on);
-				return rc;
-			} else {
-				pr_err("failed to enable backlight\n");
-				goto fail_gpio2;
-			}
-
-fail_gpio2:
-		gpio_free(GPIO_BACKLIGHT_EN);
-fail_gpio1:
-		gpio_free(GPIO_DISPLAY_PWR_EN);
-
-		} else {
-			rc = gpio_request(GPIO_FFA_LCD_PWR_EN_N,
-				"gpio_disp_pwr");
-			if (rc < 0) {
-				pr_err("failed to request lcd pwr\n");
-				return rc;
-			}
-			if (!gpio_direction_output(GPIO_FFA_LCD_PWR_EN_N,
-				1)) {
-				gpio_set_value_cansleep(GPIO_FFA_LCD_PWR_EN_N,
-					on);
-				return rc;
-			} else {
-				pr_err("failed to enable lcd pwr\n");
-				return rc;
-			}
-
-		}
-	}
-
-	return rc;
-}
-
-#define MDP_303_VSYNC_GPIO 97
-
-#ifdef CONFIG_FB_MSM_MDP303
-static struct mipi_dsi_platform_data mipi_dsi_pdata = {
-	.vsync_gpio = MDP_303_VSYNC_GPIO,
-	.dsi_power_save   = mipi_dsi_panel_power,
-	.dsi_client_reset = msm_fb_dsi_client_reset,
-};
-#endif
-
-static void __init msm_fb_add_devices(void)
-{
-	msm_fb_register_device("mdp", &mdp_pdata);
-	msm_fb_register_device("lcdc", &lcdc_pdata);
-	msm_fb_register_device("mipi_dsi", &mipi_dsi_pdata);
 }
 
 #define MSM_EBI2_PHYS			0xa0d00000
@@ -1999,7 +1651,6 @@ static void __init msm7x2x_init(void)
 		msm7x27a_cfg_smsc911x();
 		platform_add_devices(surf_ffa_devices,
 				ARRAY_SIZE(surf_ffa_devices));
-		msm_fb_add_devices();
 #ifdef CONFIG_USB_EHCI_MSM_72K
 		msm7x2x_init_host();
 #endif
