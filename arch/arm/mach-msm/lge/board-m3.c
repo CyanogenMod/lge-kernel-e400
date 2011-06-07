0799cf99d8a2ea249eead3ada16a129a5423ada4
Han Jun-Yeong <junyeong.han@lge.com>
2011-06-07 11:23:59 +0900

    [M3-EU][pmem] Remove unused pmem(audio, ebi1) macro. [TD] N/A [Desc] We use lge common pmem size in various model at this time. If you want to optimize pmem, set pmem size in the model board file.

--- arch/arm/mach-msm/lge/board-m3.c
+++ arch/arm/mach-msm/lge/board-m3.c
@@ -48,8 +48,6 @@
 
 #include "board-m3.h"
 
-#define PMEM_KERNEL_EBI1_SIZE	0x3A000
-#define MSM_PMEM_AUDIO_SIZE	0x5B000
 #define BAHAMA_SLAVE_ID_FM_ADDR         0x2A
 #define BAHAMA_SLAVE_ID_QMEMBIST_ADDR   0x7B
 #define FM_GPIO	83
