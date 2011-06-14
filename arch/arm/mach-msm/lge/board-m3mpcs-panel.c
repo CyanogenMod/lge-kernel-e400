bb3a3c465696354870a97f226e90d8225ca5192d
Han Jun-Yeong <junyeong.han@lge.com>
2011-06-14 20:22:02 +0900

    [M3-CDMA][sensor] Use one i2c line for backlight and sub-pmic. [TD] NA [0] Integrate i2c devices for backlight and sub-pmic to use one i2c line. It is very dangerous so HW will be modified in Rev.B to use separate i2c line individual sensor.

--- arch/arm/mach-msm/lge/board-m3mpcs-panel.c
+++ arch/arm/mach-msm/lge/board-m3mpcs-panel.c
@@ -1,5 +1,7 @@
 #include <linux/err.h>
 #include <linux/gpio.h>
+#include <linux/regulator/rt8053.h>
+#include <linux/regulator/consumer.h>
 
 #include <asm/mach-types.h>
 
@@ -12,6 +14,51 @@
 #include <mach/board_lge.h>
 #include <mach/vreg.h>
 
+/* Sub-PMIC */
+static struct gpio_i2c_pin subpm_i2c_pin = {
+	.sda_pin = 123,
+	.scl_pin = 122,
+};
+
+static struct regulator_consumer_supply rt8053_vreg_supply[] = {
+	REGULATOR_SUPPLY("RT8053_LDO1", NULL),
+	REGULATOR_SUPPLY("RT8053_LDO2", NULL),
+	REGULATOR_SUPPLY("RT8053_LDO3", NULL),
+	REGULATOR_SUPPLY("RT8053_LDO4", NULL),
+	REGULATOR_SUPPLY("RT8053_LDO5", NULL),
+	REGULATOR_SUPPLY("RT8053_DCDC1", NULL),
+};
+
+#define RT8053_VREG_INIT(_id, _min_uV, _max_uV) \
+	{ \
+		.id = RT8053_##_id, \
+		.initdata = { \
+			.constraints = { \
+				.valid_modes_mask = REGULATOR_MODE_NORMAL, \
+				.valid_ops_mask = REGULATOR_CHANGE_VOLTAGE | REGULATOR_CHANGE_STATUS, \
+				.min_uV = _min_uV, \
+				.max_uV = _max_uV, \
+			}, \
+			.num_consumer_supplies = 1, \
+			.consumer_supplies = \
+				&rt8053_vreg_supply[RT8053_##_id], \
+		}, \
+	}
+
+static struct rt8053_regulator_subdev rt8053_regulators[] = {
+	RT8053_VREG_INIT(LDO1, 1200000, 3300000),
+	RT8053_VREG_INIT(LDO2, 1200000, 3300000),
+	RT8053_VREG_INIT(LDO3, 1200000, 3300000),
+	RT8053_VREG_INIT(LDO4,  800000, 2850000),
+	RT8053_VREG_INIT(LDO5, 1200000, 3300000),
+	RT8053_VREG_INIT(DCDC1, 800000, 2300000),
+};
+
+static struct rt8053_platform_data rt8053_data = {
+	.num_regulators = 6,
+	.enable_gpio = 76,
+	.regulators = rt8053_regulators,
+};
 
 /* backlight device */
 static struct gpio_i2c_pin bl_i2c_pin = {
@@ -20,28 +67,46 @@ static struct gpio_i2c_pin bl_i2c_pin = {
 	.reset_pin = 124,
 };
 
-static struct i2c_gpio_platform_data bl_i2c_pdata = {
-	.sda_is_open_drain = 0,
-	.scl_is_open_drain = 0,
-	.udelay = 2,
-};
-
-static struct platform_device bl_i2c_device = {
-	.name = "i2c-gpio",
-	.dev.platform_data = &bl_i2c_pdata,
-};
-
 static struct lge_backlight_platform_data lm3530bl_data = {
 	.gpio = 124,
 	.version = 3530,
 };
 
-static struct i2c_board_info bl_i2c_bdinfo[] = {
+/* backlight, sub-pmic common device */
+static struct i2c_gpio_platform_data bl_subpm_i2c_pdata = {
+	.sda_is_open_drain = 0,
+	.scl_is_open_drain = 0,
+	.udelay = 2,
+};
+
+static struct platform_device bl_subpm_i2c_device = {
+	.name = "i2c-gpio",
+	.dev.platform_data = &bl_subpm_i2c_pdata,
+};
+
+static struct i2c_board_info bl_subpm_i2c_bdinfo[] = {
 	[0] = {
 		I2C_BOARD_INFO("lm3530bl", 0x38),
 		.type = "lm3530bl",
+		.platform_data = &lm3530bl_data,
+	},
+	[1] = {
+		I2C_BOARD_INFO("rt8053", 0x7D),
+		.type = "rt8053",
+		.platform_data = &rt8053_data,
 	},
 };
+
+void __init msm7x27a_m3_init_i2c_backlight_subpm(int bus_num)
+{
+	bl_subpm_i2c_device.id = bus_num;
+
+	/* workaround for HDK rev_a no pullup */
+	lge_init_gpio_i2c_pin_pullup(&bl_subpm_i2c_pdata, bl_i2c_pin, &bl_subpm_i2c_bdinfo[0]);
+	lge_init_gpio_i2c_pin(&bl_subpm_i2c_pdata, subpm_i2c_pin, &bl_subpm_i2c_bdinfo[1]);
+	i2c_register_board_info(bus_num, bl_subpm_i2c_bdinfo, ARRAY_SIZE(bl_subpm_i2c_bdinfo));
+	platform_device_register(&bl_subpm_i2c_device);
+}
 
 static struct platform_device mipi_dsi_r61529_panel_device = {
 	.name = "mipi_r61529",
@@ -141,21 +206,10 @@ static void __init msm_fb_add_devices(void)
 	msm_fb_register_device("mipi_dsi", &mipi_dsi_pdata);
 }
 
-void __init msm7x27a_m3_init_i2c_backlight(int bus_num)
-{
-	bl_i2c_device.id = bus_num;
-	bl_i2c_bdinfo[0].platform_data = &lm3530bl_data;
-	
-	/* workaround for HDK rev_a no pullup */
-	lge_init_gpio_i2c_pin_pullup(&bl_i2c_pdata, bl_i2c_pin, &bl_i2c_bdinfo[0]);
-	i2c_register_board_info(bus_num, &bl_i2c_bdinfo[0], 1);
-	platform_device_register(&bl_i2c_device);
-}
-
 void __init lge_add_lcd_devices(void)
 {
 	platform_add_devices(m3_panel_devices, ARRAY_SIZE(m3_panel_devices));
 	msm_fb_add_devices();
-	lge_add_gpio_i2c_device(msm7x27a_m3_init_i2c_backlight);
+	lge_add_gpio_i2c_device(msm7x27a_m3_init_i2c_backlight_subpm);
 }
 
