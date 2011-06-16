299d95afd4a1d9a96ae412b59574d0ea92e64388
Bongkyu Kim <draca98@gmail.com>
2011-06-16 19:37:56 +0530

    [7X27A_HDK][merge] 1040 merge to board files [TD] N/A [0] This commit is just copied from m3 board work.
    Change-Id: Ie0e266760987103391e58165c5afb79c19151053

--- arch/arm/mach-msm/lge/board-msm7x27a_hdk.c
+++ arch/arm/mach-msm/lge/board-msm7x27a_hdk.c
@@ -1045,41 +1045,27 @@ static int __init board_serialno_setup(char *serialno)
 __setup("androidboot.serialno=", board_serialno_setup);
 
 #ifdef CONFIG_USB_EHCI_MSM_72K
-static int  msm_hsusb_vbus_init(int on)
-{
-	int rc = 0;
-	unsigned gpio;
-
-	gpio = GPIO_HOST_VBUS_EN;
-
-	if (on) {
-		rc = gpio_request(gpio, "i2c_host_vbus_en");
-		if (rc < 0) {
-			pr_err("failed to request %d GPIO\n", gpio);
-			return rc;
-		}
-		gpio_direction_output(gpio, 1);
-	} else {
-		gpio_free(gpio);
-	}
-
-	return rc;
-}
 static void msm_hsusb_vbus_power(unsigned phy_info, int on)
 {
 #if 0
+	int rc = 0
 	unsigned gpio;
 
 	gpio = GPIO_HOST_VBUS_EN;
 
+	rc = gpio_request(gpio, "i2c_host_vbus_en");
+	if (rc < 0) {
+		pr_err("failed to request %d GPIO\n", gpio);
+		return;
+	}
+	gpio_direction_output(gpio, !!on);
 	gpio_set_value_cansleep(gpio, !!on);
+	gpio_free(gpio);
 #endif
 }
 
 static struct msm_usb_host_platform_data msm_usb_host_pdata = {
 	.phy_info       = (USB_PHY_INTEGRATED | USB_PHY_MODEL_45NM),
-	.vbus_init	= msm_hsusb_vbus_init,
-	.vbus_power		= msm_hsusb_vbus_power,
 };
 
 static void __init msm7x2x_init_host(void)
@@ -1127,6 +1113,20 @@ static int msm_hsusb_ldo_enable(int enable)
 
 	return vreg_disable(vreg_3p3);
 }
+
+#ifndef CONFIG_USB_EHCI_MSM_72K
+static int msm_hsusb_pmic_notif_init(void (*callback)(int online), int init)
+{
+	int ret = 0;
+
+	if (init)
+		ret = msm_pm_app_rpc_init(callback);
+	else
+		msm_pm_app_rpc_deinit(callback);
+
+	return ret;
+}
+#endif
 
 static struct msm_otg_platform_data msm_otg_pdata = {
 #ifndef CONFIG_USB_EHCI_MSM_72K
@@ -1307,6 +1307,8 @@ static void __init msm7x2x_init(void)
 	bt_power_init();
 #endif
 #endif /* CONFIG_MACH_LGE */
+	/*7x25a kgsl initializations*/
+	msm7x25a_kgsl_3d0_init();
 
 	platform_device_register(&msm_device_uart3);
 
