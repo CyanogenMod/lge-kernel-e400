rmcc/commtiva-kernel-z71

msm: dma: vote for ebi1 clock
Previously if we entered power collapse while an ADM transfer was
occuring, the RPM could put EBI into self-refresh. ADM accesses to EBI
during this time would cause bus errors, which would make the transfer
fail. Vote for ebi1_clk during transfers so this doesn't happen.

CRs-Fixed: 288571
Change-Id: I673250c0a2f5cb25faec0141c5661ef609ae1610
Signed-off-by: Jeff Ohlstein <johlstei@codeaurora.org>

arch/arm/mach-msm/dma.c
@@ -64,6 +64,7 @@ struct msm_dmov_conf {
 	unsigned int irq;
 	struct clk *clk;
 	struct clk *pclk;
+	struct clk *ebiclk;
 	unsigned int clk_ctl;
 	struct timer_list timer;
 };
@@ -230,13 +231,25 @@ static int msm_dmov_clk_toggle(int adm, int on)
 			goto err;
 		if (dmov_conf[adm].pclk) {
 			ret = clk_enable(dmov_conf[adm].pclk);
-			if (ret)
+			if (ret) {
 				clk_disable(dmov_conf[adm].clk);
+				goto err;
+			}
+		}
+		if (dmov_conf[adm].ebiclk) {
+			ret = clk_enable(dmov_conf[adm].ebiclk);
+			if (ret) {
+				if (dmov_conf[adm].pclk)
+					clk_disable(dmov_conf[adm].pclk);
+				clk_disable(dmov_conf[adm].clk);
+			}
 		}
 	} else {
 		clk_disable(dmov_conf[adm].clk);
 		if (dmov_conf[adm].pclk)
 			clk_disable(dmov_conf[adm].pclk);
+		if (dmov_conf[adm].ebiclk)
+			clk_disable(dmov_conf[adm].ebiclk);
 	}
 err:
 	return ret;
@@ -675,6 +688,8 @@ static struct dev_pm_ops msm_dmov_dev_pm_ops = {
 static int msm_dmov_init_clocks(struct platform_device *pdev)
 {
 	int adm = (pdev->id >= 0) ? pdev->id : 0;
+	int ret;
+
 	dmov_conf[adm].clk = clk_get(&pdev->dev, "adm_clk");
 	if (IS_ERR(dmov_conf[adm].clk)) {
 		printk(KERN_ERR "%s: Error getting adm_clk\n", __func__);
@@ -686,6 +701,16 @@ static int msm_dmov_init_clocks(struct platform_device *pdev)
 	if (IS_ERR(dmov_conf[adm].pclk)) {
 		dmov_conf[adm].pclk = NULL;
 		/* pclk not present on all SoCs, don't bail on failure */
+	}
+
+	dmov_conf[adm].ebiclk = clk_get(&pdev->dev, "ebi1_adm_clk");
+	if (IS_ERR(dmov_conf[adm].ebiclk)) {
+		dmov_conf[adm].ebiclk = NULL;
+		/* ebiclk not present on all SoCs, don't bail on failure */
+	} else {
+		ret = clk_set_rate(dmov_conf[adm].ebiclk, 27000000);
+		if (ret)
+			return -ENOENT;
 	}
 
 	return 0;
@@ -742,8 +767,10 @@ static int msm_dmov_probe(struct platform_device *pdev)
 	}
 	disable_irq(dmov_conf[adm].irq);
 	ret = msm_dmov_init_clocks(pdev);
-	if (ret)
+	if (ret) {
+		PRINT_ERROR("Requesting ADM%d clocks failed\n", adm);
 		return -ENOENT;
+	}
 
 	config_datamover(adm);
 	for (i = 0; i < MSM_DMOV_CHANNEL_COUNT; i++) {

CyanogenMod/lge-kernel-e400

Change-Id: I860cae71a236cdbd4e1ddf707607531a6205d99a

arch/arm/mach-msm/dma.c
@@ -64,6 +64,7 @@ struct msm_dmov_conf {
 	unsigned int irq;
 	struct clk *clk;
 	struct clk *pclk;
+	struct clk *ebiclk;
 	unsigned int clk_ctl;
 	struct timer_list timer;
 };
@@ -230,13 +231,25 @@ static int msm_dmov_clk_toggle(int adm, int on)
 			goto err;
 		if (dmov_conf[adm].pclk) {
 			ret = clk_enable(dmov_conf[adm].pclk);
-			if (ret)
+			if (ret) {
 				clk_disable(dmov_conf[adm].clk);
+				goto err;
+			}
+		}
+		if (dmov_conf[adm].ebiclk) {
+			ret = clk_enable(dmov_conf[adm].ebiclk);
+			if (ret) {
+				if (dmov_conf[adm].pclk)
+					clk_disable(dmov_conf[adm].pclk);
+				clk_disable(dmov_conf[adm].clk);
+			}
 		}
 	} else {
 		clk_disable(dmov_conf[adm].clk);
 		if (dmov_conf[adm].pclk)
 			clk_disable(dmov_conf[adm].pclk);
+		if (dmov_conf[adm].ebiclk)
+			clk_disable(dmov_conf[adm].ebiclk);
 	}
 err:
 	return ret;
@@ -675,6 +688,8 @@ static struct dev_pm_ops msm_dmov_dev_pm_ops = {
 static int msm_dmov_init_clocks(struct platform_device *pdev)
 {
 	int adm = (pdev->id >= 0) ? pdev->id : 0;
+	int ret;
+
 	dmov_conf[adm].clk = clk_get(&pdev->dev, "adm_clk");
 	if (IS_ERR(dmov_conf[adm].clk)) {
 		printk(KERN_ERR "%s: Error getting adm_clk\n", __func__);
@@ -686,6 +701,16 @@ static int msm_dmov_init_clocks(struct platform_device *pdev)
 	if (IS_ERR(dmov_conf[adm].pclk)) {
 		dmov_conf[adm].pclk = NULL;
 		/* pclk not present on all SoCs, don't bail on failure */
+	}
+
+	dmov_conf[adm].ebiclk = clk_get(&pdev->dev, "ebi1_clk");
+	if (IS_ERR(dmov_conf[adm].ebiclk)) {
+		dmov_conf[adm].ebiclk = NULL;
+		/* ebiclk not present on all SoCs, don't bail on failure */
+	} else {
+		ret = clk_set_rate(dmov_conf[adm].ebiclk, 27000000);
+		if (ret)
+			return -ENOENT;
 	}
 
 	return 0;
@@ -742,8 +767,10 @@ static int msm_dmov_probe(struct platform_device *pdev)
 	}
 	disable_irq(dmov_conf[adm].irq);
 	ret = msm_dmov_init_clocks(pdev);
-	if (ret)
+	if (ret) {
+		PRINT_ERROR("Requesting ADM%d clocks failed\n", adm);
 		return -ENOENT;
+	}
 
 	config_datamover(adm);
 	for (i = 0; i < MSM_DMOV_CHANNEL_COUNT; i++) {