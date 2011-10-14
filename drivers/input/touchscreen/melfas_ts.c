3f83dac160b349175675e7d2b393a6f4458cd1ab
jumok.ha <jumok.ha@lge.com>
2011-10-14 08:10:20 +0100

    [M3-COMMON]touch driver fixed for M3 EU,MPCS, VIVO to work properly
    Change-Id: I95fdeaef053dae5f9c69309905b4c3ed21ad1225

--- drivers/input/touchscreen/melfas_ts.c
+++ drivers/input/touchscreen/melfas_ts.c
@@ -46,7 +46,7 @@
 #define CORE_FIRMWARE_VERSION   0xF3
 
 #define TS_LATEST_FW_VERSION_A	0x18
-#define TS_LATEST_FW_VERSION_B	0x19
+#define TS_LATEST_FW_VERSION_B	0x1d //0x1b
 #define TS_READ_REGS_LEN 		66
 #define MELFAS_MAX_TOUCH		5
 
@@ -444,9 +444,15 @@ static int melfas_ts_probe(struct i2c_client *client, const struct i2c_device_id
 	if(ret < 0){
 		printk(KERN_ERR "melfas_ts_work_func : i2c_master_recv [%d]\n", ret);			
 	}
+#if defined(CONFIG_MACH_MSM7X27A_M3EU) 	
+	mcsdl_download_binary_data(1, 1,hw_ver,0x01);
+#elif defined(CONFIG_MACH_MSM7X27A_M3MPCS)
 	if (fw_ver !=TS_LATEST_FW_VERSION_A && fw_ver !=TS_LATEST_FW_VERSION_B) {
 		mcsdl_download_binary_data(1, 1,hw_ver,comp_ver);
 	} 
+#else 	
+	mcsdl_download_binary_data(1, 1,hw_ver,0x00);
+#endif
 #endif // SET_DOWNLOAD_BY_GPIO
 	
 	enable_irq(ts->num_irq);
