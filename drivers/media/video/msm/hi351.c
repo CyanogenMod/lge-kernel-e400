1092ea49e458f351be261be513e80f5741b0f87c
hong.junki <hong.junki@lge.com>
2011-12-17 15:03:27 +0900

    [E0][CAMERA]add delay for safety on I2C
    Change-Id: I97370b30f3c9ef5978d1feaca4c9f44d9889f1b3

--- drivers/media/video/msm/hi351.c
+++ drivers/media/video/msm/hi351.c
@@ -132,6 +132,7 @@ static int32_t hi351_i2c_txdata(u16 saddr,
 		},
 	};
 
+	udelay(15);		//for i2c stabilize
 #if SENSOR_DEBUG
 	pr_err("hi351_i2c_txdata: addr : 0x%x len : %d buf[0]: 0x%x\n", msg[0].addr, msg[0].len, *(msg[0].buf));
 
